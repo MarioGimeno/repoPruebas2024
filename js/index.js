5371eca29eafce0665ad94f101cbc88ab370cc26
MarioGimeno <mgimenonavaz@hotmail.com>
2024-05-10 13:26:52 +0200

    Commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-const valor = document.getElementById("textBox");
-const boton = document.getElementById("showColor");
-const divColor = document.getElementById("color");
+    const valor = document.getElementById("textBox");
+    const boton = document.getElementById("showColor");
+    const divColor = document.getElementById("color");
 
-function cambiarColor(color) {
-    divColor.style.backgroundColor = color;
-};
+    function cambiarColor() {
+        divColor.style.backgroundColor = valor.value;
+    }
 
-boton.addEventListener("click", cambiarColor(valor.value));
+    boton.addEventListener("click", cambiarColor);
 });
